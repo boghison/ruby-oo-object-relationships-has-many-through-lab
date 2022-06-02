a6f606bb411bd73fb5fedfdc2e8f33aac61342a6
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-02 20:31:25 +0300

    commit tree be1c50d6dfe56945e0b6d1ba3c109274be756c29

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,26 @@
+class Doctor
+    @@all = []
 
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select {|app| app.doctor == self}
+    end
+
+    def patients
+        appointments.collect {|app| app.patient}.uniq
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,27 @@
+class Patient
+    @@all = []
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|app| app.patient == self}
+    end
+
+    def doctors
+        appointments.collect {|app| app.doctor}.uniq
+    end
+end
 
